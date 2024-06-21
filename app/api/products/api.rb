1690e6c110c1b2b6e126902eb70e5e29fe88a052
lada000 <lavacashy.sinkevich@gmail.com>
2024-06-21 12:46:18 +0200

    add products routes

--- app/api/products/api.rb
+++ app/api/products/api.rb
@@ -2,9 +2,6 @@ require 'grape'
 
 module Products
   class API < Grape::API
-    version 'v1', using: :path
-    format :json
-
     resource :products do
       desc 'Return all products'
       get do
@@ -23,14 +20,65 @@ module Products
 
       desc 'Create a product'
       params do
-        requires :name, type: String, desc: 'Product Name'
-        requires :price, type: Float, desc: 'Product Price'
+        requires :title, type: String
+        requires :description, type: String
+        requires :price, type: Float
+        requires :currency, type: String
+        requires :status, type: String
+        requires :category_id, type: Integer
+        optional :user_id, type: Integer
       end
       post do
-        Product.create!(
-          name: params[:name],
-          price: params[:price]
-        )
+        Product.create!({
+          title: params[:title],
+          description: params[:description],
+          price: params[:price],
+          currency: params[:currency],
+          status: params[:status],
+          category_id: params[:category_id],
+          user_id: params[:user_id]
+        })
+      end
+
+      desc 'Update a product'
+      params do
+        requires :id, type: Integer
+        requires :title, type: String
+        requires :description, type: String
+        requires :price, type: Float
+        requires :currency, type: String
+        requires :status, type: String
+        requires :category_id, type: Integer
+        optional :user_id, type: Integer
+      end
+      put ':id' do
+        product = Product.find(params[:id])
+        product.update({
+          title: params[:title],
+          description: params[:description],
+          price: params[:price],
+          currency: params[:currency],
+          status: params[:status],
+          category_id: params[:category_id],
+          user_id: params[:user_id]
+        })
+        product
+      end
+
+      desc 'Delete a product'
+      params do
+        requires :id, type: Integer
+      end
+      delete ':id' do
+        Product.find(params[:id]).destroy
+      end
+
+      desc 'Return products for a specific user'
+      params do
+        requires :user_id, type: Integer, desc: 'User ID'
+      end
+      get 'user_products' do
+        Product.where(user_id: params[:user_id])
       end
     end
   end
